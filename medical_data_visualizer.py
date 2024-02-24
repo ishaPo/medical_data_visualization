9bb05bd9c912685eb6b7f8db3698abbe0ec3751b
Isha Syed <isha.syed2003@gmail.com>
2024-02-24 17:38:57 +0000

    changes by isha: medical data main file

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -12,6 +12,7 @@ df['bmi'] = df['weight'] / (df['height_m'] ** 2)
 df['overweight'] = (df['bmi'] > 25).astype(int)
 df.drop(['height_m', 'bmi'], axis=1, inplace=True)
 
+
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
 df['cholesterol'] = df['cholesterol'].apply(lambda x: 1 if x > 1 else 0)
 df['gluc'] = df['gluc'].apply(lambda x: 1 if x > 1 else 0)
